08bfae460d4396f78ab7753a7edfb8aff63c42a2
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-09-25 12:32:58 +0300

    01.Introduction-to-Haskell 2

--- 01.Introduction-to-Haskell/main.hs
+++ 01.Introduction-to-Haskell/main.hs
@@ -1,8 +1,27 @@
-doubleVar :: (Num a) => a -> a
-doubleVar val = val + val
+--01
+doubleVal :: (Num a) => a -> a
+doubleVal val = val + val
 
+--02
 isEven :: (Eq a, Integral a) => a -> Bool
 isEven val = mod val 2 == 0
 
+--03
 biggestOf3 :: (Num a, Ord a) => a -> a -> a -> a
 biggestOf3 a b c = max (max a b) c
+
+--04
+add1 :: (Num a) => a -> a
+add1 a = a + 1
+
+remove1 :: (Num a) => a -> a
+remove1 a = a - 1
+
+execute :: (Num a) => (a -> a) -> a -> a
+execute command a = command a
+
+--05
+factorial :: (Num a, Ord a) => a -> a -> a -> a
+factorial n result i
+  | i <= n    = factorial n (result * i) (i + 1)
+  | otherwise = result
